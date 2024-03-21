3500334988a2bd4dee80945a6d5fbca953556b6b
Cassandra Rusk <149620042+C-Rusk@users.noreply.github.com>
2024-03-21 13:43:39 -0500

    My first JavaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+// Write your JavaScript below this line
+var firstName = "Cassandra";
+var favoriteSnack = "chocolate";
+
+console.log(
+  "Hi, my name is " +
+    firstName +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now. "
+);
+
+var numberOfHorses = 3;
+console.log(numberOfHorses);
+
+var favoriteFlower = "daisies";
+console.log("My favorite flower is " + favoriteFlower);
+
+var city = "Las Vegas";
+var birthday = "May 12";
+
+console.log("I was born in " + city + " on " + birthday);
